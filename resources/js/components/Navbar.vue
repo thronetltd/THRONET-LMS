ee71fa9ac0bece8323c49effeca1711d2be38aa5
thronet <thronetltd@outlook.com>
2026-03-20 20:21:00 +0300

    Create Navbar component with THRONET logo

---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav>
+    <img src="/path/to/thronet-logo.png" alt="THRONET Logo" />
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+};
+</script>
+
+<style scoped>
+nav {
+  display: flex;
+  align-items: center;
+}
+
+img {
+  height: 50px;  /* Adjust as necessary */
+}
+</style>
